9663a6912cda345250774ce496487974090e52d2
Francesco Grigoli <1344902+wulwife@users.noreply.github.com>
2022-06-20 16:39:12 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+setup(name="latlon2cart",
+      version="1.0",
+      description="Conversion between Geographic and General Cartesian Coordinates",
+      author="Francesco Grigoli",
+      author_email="francesco.grigoli@unipi.it",
+      py_modules=["latlon2cart"])
+
